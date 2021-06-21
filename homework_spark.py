7d6d0dac1dcfbf4c0369ac56ab9d3393a542147f
Arinan Najah Putra <34894631+arinannp@users.noreply.github.com>
2021-06-21 09:52:17 +0700

    Update homework_spark.py

--- homework_spark.py
+++ homework_spark.py
@@ -2,16 +2,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
 
+spark = SparkSession.builder.appName('SparkHomework').getOrCreate()
 
-if __file__ == "__main__":
-    
-    spark = SparkSession.builder \
-        .builder \
-        .appName("HomeworkSparkCode") \
-        .getOrCreate()
+df = spark.read.format("json").option("multiline","true").json("data/example2.json")
+dfRenamed = df.withColumnRenamed("value", "hexcolor")
+dfSorted = dfRenamed.sort(length(col("color")), ascending=True)
 
-    df = spark.read.format("json").option("multiline","true").json("data/example2.json")
-    dfRenamed = df.withColumnRenamed("value", "hexcolor")
-    dfSorted = dfRenamed.sort(length(col("color")), ascending=True)
-    
-    dfSorted.show(truncate=False)
+dfSorted.show(truncate=False)
